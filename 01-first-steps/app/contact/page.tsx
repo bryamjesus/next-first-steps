5b05ee0a4437166f389f5c3d5970c4664be623fd
Bryam Talledo <bryam.talledo@outlook.com>
2024-12-09 16:58:17 -0500

    Feat: update Metadata

--- 01-first-steps/app/contact/page.tsx
+++ 01-first-steps/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
   return (
     <div className="flex flex-col items-center p-24">
-      <span className="text-5xl">Page Contact</span>
+      <span className="text-5xl">Contact Page</span>
     </div>
   );
 }
--- 01-first-steps/app/pricing/page.tsx
+++ 01-first-steps/app/pricing/page.tsx
@@ -1,7 +1,7 @@
 export default function PricingPage() {
   return (
     <div className="flex flex-col items-center p-24">
-      <span className="text-5xl">Page Pricing!</span>
+      <span className="text-5xl">Pricing Page</span>
     </div>
   );
 }
