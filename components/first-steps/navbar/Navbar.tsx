078c1e52660fd9c471ac19551e85db226f9064c3
Bryam Talledo <bryam.talledo@outlook.com>
2024-12-09 23:01:52 -0500

    FEAT: Server Components - Async await

--- components/first-steps/navbar/Navbar.tsx
+++ components/first-steps/navbar/Navbar.tsx
@@ -1,5 +1,22 @@
+// Solo se usara cuando se quiera que se reenderize
+// en el navegador
+// 'use client'
+
+// const temporalAsync = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, 51000);
+//   });
+// };
+
+// export const NavBar = async () => {
 export const NavBar = () => {
+  // este mensje saldra en el servidor
+  // a menos que se use 'use cliente'
   console.log('Navbar creado');
+
+  // await temporalAsync;
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <span>Home</span>
