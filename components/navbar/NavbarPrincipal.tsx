23427e2588dca88403b34e86738fd0970b9eb4cc
Bryam Talledo <bryam.talledo@outlook.com>
2024-12-10 23:44:08 -0500

    Fix: update NAVBAR general

--- components/navbar/NavbarPrincipal.tsx
+++ components/navbar/NavbarPrincipal.tsx
@@ -1,19 +1,26 @@
+import { HomeIcon } from '@primer/octicons-react';
 import Link from 'next/link';
+
+const navItems = [
+  { path: '/first-steps/', text: 'Firs steps' },
+  { path: '/', text: 'Second steps' },
+  { path: '/', text: 'etc' },
+];
 
 export const NavBarPrincipal = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={'/'} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
       <div className="flex flex-1"></div>
-      <Link className="mr-2" href="/first-steps">
-        Firs steps
-      </Link>
-      <Link className="mr-2" href="/">
-        Second steps
-      </Link>
-      <Link className="mr-2" href="/">
-        Contact
-      </Link>
+      {navItems.map((navItem) => (
+        // TODO: luego cambiar el Key al navItem.path
+        <Link key={navItem.text} className="mr-2" href={navItem.path}>
+          {navItem.text}
+        </Link>
+      ))}
     </nav>
   );
 };
