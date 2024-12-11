20e8abb3eaf3b0a689751e1c62b14c98f02ba305
Bryam Talledo <bryam.talledo@outlook.com>
2024-12-10 23:21:35 -0500

    Feat: Next/Link

--- components/first-steps/navbar/Navbar.tsx
+++ components/first-steps/navbar/Navbar.tsx
@@ -1,3 +1,44 @@
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+
+const navItems = [
+  { path: '/first-steps/about', text: 'About' },
+  { path: '/first-steps/pricing', text: 'Pricing' },
+  { path: '/first-steps/contact', text: 'Contact' },
+];
+
+export const NavBar = () => {
+  // este mensje saldra en el servidor
+  // a menos que se use 'use cliente'
+  console.log('Navbar creado');
+
+  return (
+    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <Link href={'/first-steps'} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
+      <div className="flex flex-1"></div>
+
+      {navItems.map((navItem) => (
+        <Link key={navItem.path} className="mr-2" href={navItem.path}>
+          {navItem.text}
+        </Link>
+      ))}
+
+      {/* <Link className="mr-2" href="/first-steps/about">
+        About
+      </Link>
+      <Link className="mr-2" href="/first-steps/pricing">
+        Pricing
+      </Link>
+      <Link className="mr-2" href="/first-steps/contact">
+        Contact
+      </Link> */}
+    </nav>
+  );
+};
+
 // Solo se usara cuando se quiera que se reenderize
 // en el navegador
 // 'use client'
@@ -11,25 +52,5 @@
 // };
 
 // export const NavBar = async () => {
-export const NavBar = () => {
-  // este mensje saldra en el servidor
-  // a menos que se use 'use cliente'
-  console.log('Navbar creado');
-
-  // await temporalAsync;
-  return (
-    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
-      <div className="flex flex-1"></div>
-      <a className="mr-2" href="/first-steps/about">
-        About
-      </a>
-      <a className="mr-2" href="/first-steps/pricing">
-        Pricing
-      </a>
-      <a className="mr-2" href="/first-steps/contact">
-        Contact
-      </a>
-    </nav>
-  );
-};
+// await temporalAsync;
+// }
